af33e2f3cea230eb71bef275ab6b63e8dabe9778
thyrrel <thyrrel@gmail.com>
2025-08-21 17:31:44 -0300

    MainActivity.kt

--- android/app/main/kotlin/com/thyrrel/observador/MainActivity.kt
+++ android/app/main/kotlin/com/thyrrel/observador/MainActivity.kt
@@ -2,4 +2,6 @@ package com.thyrrel.observador
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    // Nenhum código extra é necessário aqui para o embedding V2
+}
