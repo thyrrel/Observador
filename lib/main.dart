6fe9ff02944d9d4682f45081f5a813726191f383
thyrrel <thyrrel@gmail.com>
2025-08-21 00:51:27 -0300

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'pages/home_page.dart';   // adicione essa linha no topo
+ 'package:flutter/material.dart';
 
 void main() {
   runApp(const ObservadorApp());
