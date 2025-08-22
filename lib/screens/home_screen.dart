3f4a66abd770cc9e555882232f773801bf94cd9f
thyrrel <thyrrel@gmail.com>
2025-08-22 15:07:32 -0300

    home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Observador'),
+      ),
+      body: const Center(
+        child: Text('Bem-vindo ao Observador!'),
+      ),
+    );
+  }
+}
