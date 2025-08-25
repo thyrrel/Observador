4fde298b3fbcde3c2a0bff64dd9435abecfd6368
thyrrel <thyrrel@gmail.com>
2025-08-24 22:20:36 -0300

    routers_credentials_service.dart

--- lib/services/routers_credentials_service.dart
+++ lib/services/routers_credentials_service.dart
@@ -1,14 +1,18 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class WifiCredentialsService {
-  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+class RouterCredentialsService {
+  final _storage = const FlutterSecureStorage();
 
-  Future<void> saveCredentials(String ssid, String password) async =>
-      await _storage.write(key: 'wifi_$ssid', value: password);
+  Future<void> saveCredentials(String routerIP, String username, String password) async {
+    await _storage.write(key: 'router_$routerIP', value: '$username|$password');
+  }
 
-  Future<String?> getPassword(String ssid) async =>
-      await _storage.read(key: 'wifi_$ssid');
-
-  Future<void> deleteCredentials(String ssid) async =>
-      await _storage.delete(key: 'wifi_$ssid');
+  Future<Map<String, String>?> getCredentials(String routerIP) async {
+    String? data = await _storage.read(key: 'router_$routerIP');
+    if (data != null) {
+      var parts = data.split('|');
+      return {'username': parts[0], 'password': parts[1]};
+    }
+    return null;
+  }
 }
