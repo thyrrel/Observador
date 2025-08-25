0bda1e8af931ca10254c0a5967332e1cd202ea7c
thyrrel <thyrrel@gmail.com>
2025-08-24 21:49:48 -0300

    ia_service.dart

--- lib/services/ia_service.dart
+++ lib/services/ia_service.dart
@@ -1,53 +1,28 @@
-import 'dart:async';
 import '../models/device_model.dart';
 
-typedef VoiceCallback = void Function(String message);
-
 class IAService {
-  final VoiceCallback? voiceCallback;
+  final Function(String message) voiceCallback;
 
-  IAService({this.voiceCallback});
+  IAService({required this.voiceCallback});
 
-  List<String> allowedMacs = [];
-
-  List<DeviceModel> analyzeDevices(List<DeviceModel> devices) {
-    List<DeviceModel> alerts = [];
-    for (var device in devices) {
-      if (!allowedMacs.contains(device.mac)) {
-        alerts.add(device);
-        _notifyVoice(
-            "Dispositivo suspeito detectado: ${device.name}, fabricante: ${device.manufacturer}");
+  void analyzeDevices(List<DeviceModel> devices) {
+    for (var d in devices) {
+      if (d.type == 'Desconhecido') {
+        _notifyVoice('Dispositivo suspeito detectado: ${d.mac}');
       }
     }
-    return alerts;
   }
 
   void analyzeTraffic(List<DeviceModel> devices, Map<String, double> usageMbps) {
-    devices.forEach((device) {
-      if (device.type.contains("TV") && usageMbps[device.ip] != null) {
-        double bandwidth = usageMbps[device.ip]!;
-        if (bandwidth > 20) {
-          _notifyVoice(
-              "A TV está consumindo $bandwidth Mbps. Quer priorizar a rede para seu jogo?");
-        }
+    for (var d in devices) {
+      double mbps = usageMbps[d.ip] ?? 0;
+      if (mbps > 20 && d.type.contains("TV")) {
+        _notifyVoice('A TV ${d.name} está consumindo $mbps Mbps. Deseja priorizar seu jogo?');
       }
-    });
-  }
-
-  void predictBehavior(List<DeviceModel> devices, Map<String, DateTime> lastActive) {
-    final now = DateTime.now();
-    devices.forEach((device) {
-      if (lastActive.containsKey(device.mac)) {
-        final diff = now.difference(lastActive[device.mac]!);
-        if (diff.inMinutes > 120) {
-          _notifyVoice(
-              "O dispositivo ${device.name} não esteve ativo nas últimas 2 horas. Sem mudanças previstas.");
-        }
-      }
-    });
+    }
   }
 
   void _notifyVoice(String message) {
-    if (voiceCallback != null) voiceCallback!(message);
+    voiceCallback(message);
   }
 }
