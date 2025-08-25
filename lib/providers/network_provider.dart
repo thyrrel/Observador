0365234bf073585a949698f037910502ed31fbe5
thyrrel <thyrrel@gmail.com>
2025-08-25 04:06:41 -0300

    network_provider.dart

--- lib/providers/network_provider.dart
+++ lib/providers/network_provider.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
-import '../models/device_model.dart';
 import '../services/network_service.dart';
+import '../models/device_model.dart';
 
-class NetworkProvider extends ChangeNotifier {
+class NetworkProvider with ChangeNotifier {
   final NetworkService _networkService = NetworkService();
-  List<DeviceModel> devices = [];
 
-  Future<void> refreshDevices() async {
-    devices = await _networkService.getDevices();
+  List<DeviceModel> get devices => _networkService.getDevices();
+
+  void addDevice(DeviceModel device) {
+    _networkService.addDevice(device);
     notifyListeners();
   }
 
-  void toggleBlock(DeviceModel device) {
-    device.blocked = !device.blocked;
-    _networkService.setBlock(device.ip, device.blocked);
+  void blockIP(String ip) {
+    _networkService.blockIP(ip);
+    notifyListeners();
+  }
+
+  void setHighPriority(String ip) {
+    _networkService.setHighPriority(ip);
+    notifyListeners();
+  }
+
+  void limitIP(String ip, int limit) {
+    _networkService.limitIP(ip, limit);
     notifyListeners();
   }
 }
