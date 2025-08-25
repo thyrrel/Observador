0dbf3dedea9cb92d5ec5eb8c2b034c7d97487a32
thyrrel <thyrrel@gmail.com>
2025-08-25 03:44:03 -0300

    Update network_provider.dart

--- lib/providers/network_provider.dart
+++ lib/providers/network_provider.dart
@@ -1,6 +1,19 @@
-
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import '../models/device_model.dart';
+import '../services/network_service.dart';
 
 class NetworkProvider extends ChangeNotifier {
-  // Estado mínimo
+  final NetworkService _networkService = NetworkService();
+  List<DeviceModel> devices = [];
+
+  Future<void> refreshDevices() async {
+    devices = await _networkService.getDevices();
+    notifyListeners();
+  }
+
+  void toggleBlock(DeviceModel device) {
+    device.blocked = !device.blocked;
+    _networkService.setBlock(device.ip, device.blocked);
+    notifyListeners();
+  }
 }
