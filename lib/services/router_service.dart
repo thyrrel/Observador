aa8cc18a5ea068ba40f8e7621ecf986c4ffb9b3c
thyrrel <thyrrel@gmail.com>
2025-08-24 21:09:52 -0300

    router_service.dart

--- lib/services/router_service.dart
+++ lib/services/router_service.dart
@@ -1,68 +1,52 @@
 import 'dart:convert';
-import 'package:dartssh2/dartssh2.dart';
+import 'package:http/http.dart' as http;
+import '../models/device_model.dart';
 
 class RouterService {
-  static const _ip = '192.168.1.1';
-  static const _user = 'admin';    // virá das credenciais VIP
-  static const _pass = '1234';     // virá das credenciais VIP
+  final String routerIP;
+  final String token;
 
-  static Future<void> blockIP(String ip) async {
-    final client = SSHClient(
-      await SSHSocket.connect(_ip, 22),
-      username: _user,
-      onPasswordRequest: () => _pass,
-    );
+  RouterService({required this.routerIP, required this.token});
 
-    /// Define prioridade alta (ex.: para jogos)
-static Future<void> setHighPriority(String ip) async {
-  final client = SSHClient(
-    await SSHSocket.connect(_ip, 22),
-    username: _user,
-    onPasswordRequest: () => _pass,
-  );
+  Map<String, String> get headers => {
+    'Authorization': 'Bearer $token',
+    'Content-Type': 'application/json',
+  };
 
-  // Exemplo OpenWRT: marca pacote com DSCP 46 (Expedited Forwarding)
-  await client.run(
-    'iptables -t mangle -A POSTROUTING -s $ip -j DSCP --set-dscp 46',
-  );
-  client.close();
-}
-
-/// Limita banda para X kbit/s (ex.: 512 kbit/s)
-static Future<void> limitIP(String ip, int kbit) async {
-  final client = SSHClient(
-    await SSHSocket.connect(_ip, 22),
-    username: _user,
-    onPasswordRequest: () => _pass,
-  );
-
-  // tc + HTB no OpenWRT
-  await client.run(
-    'tc class add dev br-lan parent 1: classid 1:10 htb rate ${kbit}kbit ceil ${kbit}kbit',
-  );
-  await client.run(
-    'tc filter add dev br-lan protocol ip parent 1:0 prio 1 u32 match ip src $ip flowid 1:10',
-  );
-  client.close();
-}
-
-    
-    // Exemplo OpenWRT/DD-WRT
-    await client.run(
-      'iptables -I FORWARD -s $ip -j DROP',
-    );
-    client.close();
+  Future<List<DeviceModel>> getDevices() async {
+    final response = await http.get(Uri.parse('http://$routerIP/devices'), headers: headers);
+    if (response.statusCode == 200) {
+      final List data = jsonDecode(response.body);
+      return data.map((e) => DeviceModel.fromJson(e)).toList();
+    }
+    return [];
   }
 
-  static Future<void> unblockIP(String ip) async {
-    final client = SSHClient(
-      await SSHSocket.connect(_ip, 22),
-      username: _user,
-      onPasswordRequest: () => _pass,
+  Future<bool> blockDevice(String mac) async {
+    final response = await http.post(
+      Uri.parse('http://$routerIP/block'),
+      headers: headers,
+      body: jsonEncode({'mac': mac}),
     );
-    await client.run(
-      'iptables -D FORWARD -s $ip -j DROP',
+    return response.statusCode == 200;
+  }
+
+  Future<bool> unblockDevice(String mac) async {
+    final response = await http.post(
+      Uri.parse('http://$routerIP/unblock'),
+      headers: headers,
+      body: jsonEncode({'mac': mac}),
     );
-    client.close();
+    return response.statusCode == 200;
+  }
+
+  Future<bool> startVPN() async {
+    final response = await http.post(Uri.parse('http://$routerIP/vpn/start'), headers: headers);
+    return response.statusCode == 200;
+  }
+
+  Future<bool> stopVPN() async {
+    final response = await http.post(Uri.parse('http://$routerIP/vpn/stop'), headers: headers);
+    return response.statusCode == 200;
   }
 }
