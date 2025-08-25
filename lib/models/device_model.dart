b08a77d6241050cbb2eebac557b6a7747d2222cf
thyrrel <thyrrel@gmail.com>
2025-08-24 21:48:38 -0300

    device_model.dart

--- lib/models/device_model.dart
+++ lib/models/device_model.dart
@@ -1,35 +1,17 @@
 class DeviceModel {
-  String name;
   String ip;
   String mac;
   String manufacturer;
   String type;
+  String name;
   bool isBlocked;
 
   DeviceModel({
-    required this.name,
     required this.ip,
     required this.mac,
     required this.manufacturer,
-    this.type = "Desconhecido",
+    required this.type,
+    required this.name,
     this.isBlocked = false,
   });
-
-  Map<String, dynamic> toJson() => {
-    'name': name,
-    'ip': ip,
-    'mac': mac,
-    'manufacturer': manufacturer,
-    'type': type,
-    'isBlocked': isBlocked,
-  };
-
-  factory DeviceModel.fromJson(Map<String, dynamic> json) => DeviceModel(
-    name: json['name'],
-    ip: json['ip'],
-    mac: json['mac'],
-    manufacturer: json['manufacturer'],
-    type: json['type'],
-    isBlocked: json['isBlocked'],
-  );
 }
