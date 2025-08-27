1475aee63b1e140586fba93d8e676fca4a3b7830
thyrrel <thyrrel@gmail.com>
2025-08-27 03:21:21 -0300

    theme_service.dart

--- lib/services/theme_service.dart
+++ lib/services/theme_service.dart
@@ -1,41 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-class ThemeService {
-  static const _key = 'theme_mode';
+class ThemeService extends ChangeNotifier {
+  bool _isDarkMode = false;
 
-  static Future<ThemeMode> load() async {
-    final prefs = await SharedPreferences.getInstance();
-    final index = prefs.getInt(_key) ?? 0;
-    return ThemeMode.values[index];
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeMode get currentTheme => _isDarkMode ? ThemeMode.dark : ThemeMode.light;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
   }
-
-  static Future<void> save(ThemeMode mode) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(_key, mode.index);
-  }
-
-  static ThemeData light() => ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
-        brightness: Brightness.light,
-      );
-
-  static ThemeData dark() => ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
-        brightness: Brightness.dark,
-      );
-
-  static ThemeData matrix() => ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green,
-          brightness: Brightness.dark,
-        ),
-        scaffoldBackgroundColor: Colors.black,
-        textTheme: Typography.whiteCupertino.apply(
-          bodyColor: Colors.greenAccent,
-          displayColor: Colors.greenAccent,
-        ),
-      );
 }
