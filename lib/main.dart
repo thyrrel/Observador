60f0b0f760bfad71c1d8d8ec14aeb49f3faa9e05
thyrrel <thyrrel@gmail.com>
2025-08-27 01:22:07 -0300

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/network_provider.dart';
-import 'screens/home_screen.dart';
+import 'screens/network_screen.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => NetworkProvider()),
-      ],
-      child: const ObservadorApp(),
-    ),
-  );
+  runApp(const ObservadorApp());
 }
 
 class ObservadorApp extends StatelessWidget {
@@ -19,11 +12,16 @@ class ObservadorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Observador',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(useMaterial3: true),
-      home: const HomeScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => NetworkProvider()..loadNetworkData()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Observador',
+        theme: ThemeData.dark(useMaterial3: true),
+        home: const NetworkScreen(),
+      ),
     );
   }
 }
