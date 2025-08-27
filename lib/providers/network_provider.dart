13e9ece19f6e158a7249797fa18f8b4b9eef131b
thyrrel <thyrrel@gmail.com>
2025-08-27 03:29:12 -0300

    network_provider.dart

--- lib/providers/network_provider.dart
+++ lib/providers/network_provider.dart
@@ -3,19 +3,33 @@ import '../services/network_service.dart';
 
 class NetworkProvider extends ChangeNotifier {
   final NetworkService _networkService = NetworkService();
-  Map<String, dynamic> _networkData = {};
   bool _loading = false;
 
-  Map<String, dynamic> get networkData => _networkData;
+  List<NetworkDevice> get devices => _networkService.devices;
   bool get loading => _loading;
+
+  NetworkProvider() {
+    _networkService.addListener(notifyListeners);
+    loadNetworkData();
+  }
 
   Future<void> loadNetworkData() async {
     _loading = true;
     notifyListeners();
 
-    _networkData = await _networkService.getNetworkStatus();
+    await _networkService.scanNetwork();
 
     _loading = false;
     notifyListeners();
+  }
+
+  void toggleBlock(NetworkDevice device) {
+    _networkService.toggleBlock(device);
+  }
+
+  @override
+  void dispose() {
+    _networkService.dispose();
+    super.dispose();
   }
 }
