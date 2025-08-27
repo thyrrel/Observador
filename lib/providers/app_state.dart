fcc24694358d95aa4a01c78bf6478e58eb90f744
thyrrel <thyrrel@gmail.com>
2025-08-27 17:03:54 -0300

    Update app_state.dart

--- lib/providers/app_state.dart
+++ lib/providers/app_state.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+enum AppTheme { Light, Dark, OLED, Matrix }
+
 class AppState extends ChangeNotifier {
-  bool _darkMode = false;
+  AppTheme _theme = AppTheme.Light;
   final FlutterSecureStorage _storage = FlutterSecureStorage();
 
-  bool get darkMode => _darkMode;
+  AppTheme get theme => _theme;
+
+  ThemeData get themeData {
+    switch (_theme) {
+      case AppTheme.Dark:
+        return ThemeData.dark().copyWith(
+          primaryColor: Colors.blueGrey[800],
+          colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.cyan[300]),
+        );
+      case AppTheme.OLED:
+        return ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Colors.black,
+          primaryColor: Colors.black,
+          colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+          textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)),
+        );
+      case AppTheme.Matrix:
+        return ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Colors.black,
+          primaryColor: Colors.black,
+          colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.greenAccent),
+          textTheme: TextTheme(bodyText2: TextStyle(color: Colors.greenAccent)),
+        );
+      case AppTheme.Light:
+      default:
+        return ThemeData.light().copyWith(
+          primaryColor: Colors.blue,
+          colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.lightBlue[200]),
+        );
+    }
+  }
 
   Future<void> loadTheme() async {
-    final value = await _storage.read(key: 'darkMode');
-    _darkMode = value == 'true';
+    final value = await _storage.read(key: 'theme');
+    if (value != null) {
+      _theme = AppTheme.values.firstWhere((e) => e.toString() == value, orElse: () => AppTheme.Light);
+    }
     notifyListeners();
   }
 
-  Future<void> toggleTheme(bool value) async {
-    _darkMode = value;
-    await _storage.write(key: 'darkMode', value: value.toString());
+  Future<void> setTheme(AppTheme newTheme) async {
+    _theme = newTheme;
+    await _storage.write(key: 'theme', value: newTheme.toString());
     notifyListeners();
   }
 }
