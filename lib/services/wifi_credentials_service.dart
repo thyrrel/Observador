14f6630250848ed60a48b732b481f89f2e368752
thyrrel <thyrrel@gmail.com>
2025-08-27 15:43:40 -0300

    wifi_credentials_service.dart

--- lib/services/wifi_credentials_service.dart
+++ lib/services/wifi_credentials_service.dart
@@ -1,13 +1,2 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-class WifiCredentialsService {
-  final _storage = const FlutterSecureStorage();
-
-  Future<void> saveCredentials(String ssid, String password) async {
-    await _storage.write(key: 'wifi_$ssid', value: password);
-  }
-
-  Future<String?> getPassword(String ssid) async {
-    return await _storage.read(key: 'wifi_$ssid');
-  }
-}
+dependencies:
+  flutter_secure_storage: ^9.0.0
