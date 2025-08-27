7a29290649e982ca357fbd04d3f8dd1ef4255ae4
thyrrel <thyrrel@gmail.com>
2025-08-27 15:40:46 -0300

    network_device.dart

--- lib/models/network_device.dart
+++ lib/models/network_device.dart
@@ -1,12 +1,16 @@
 class NetworkDevice {
-  final String id;
+  final int? id;
   final String name;
   final String ip;
+  final String mac;
+  final bool blocked;
 
   NetworkDevice({
-    required this.id,
+    this.id,
     required this.name,
     required this.ip,
+    required this.mac,
+    this.blocked = false,
   });
 
   Map<String, dynamic> toMap() {
@@ -14,6 +18,18 @@ class NetworkDevice {
       'id': id,
       'name': name,
       'ip': ip,
+      'mac': mac,
+      'blocked': blocked ? 1 : 0,
     };
+  }
+
+  factory NetworkDevice.fromMap(Map<String, dynamic> map) {
+    return NetworkDevice(
+      id: map['id'],
+      name: map['name'],
+      ip: map['ip'],
+      mac: map['mac'],
+      blocked: map['blocked'] == 1,
+    );
   }
 }
