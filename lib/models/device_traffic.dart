7748a691e191b4684a76ee4159f87a873717cdf2
thyrrel <thyrrel@gmail.com>
2025-08-28 03:17:41 -0300

    Update device_traffic.dart

--- lib/models/device_traffic.dart
+++ lib/models/device_traffic.dart
@@ -1,35 +1,11 @@
 class DeviceTraffic {
-  final String ip;
-  final String mac;
-  final int bytesSent;
-  final int bytesReceived;
-  final DateTime timestamp;
+  final String deviceName;
+  final int rxBytes;
+  final int txBytes;
 
   DeviceTraffic({
-    required this.ip,
-    required this.mac,
-    required this.bytesSent,
-    required this.bytesReceived,
-    required this.timestamp,
+    required this.deviceName,
+    required this.rxBytes,
+    required this.txBytes,
   });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'ip': ip,
-      'mac': mac,
-      'bytesSent': bytesSent,
-      'bytesReceived': bytesReceived,
-      'timestamp': timestamp.toIso8601String(),
-    };
-  }
-
-  factory DeviceTraffic.fromMap(Map<String, dynamic> map) {
-    return DeviceTraffic(
-      ip: map['ip'],
-      mac: map['mac'],
-      bytesSent: map['bytesSent'],
-      bytesReceived: map['bytesReceived'],
-      timestamp: DateTime.parse(map['timestamp']),
-    );
-  }
 }
