07ee506c86ad7cf22b4ba49ff355fed8d0be597d
thyrrel <thyrrel@gmail.com>
2025-08-28 03:26:31 -0300

    Update dashboard_page.dart

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,9 +1,8 @@
 // lib/pages/dashboard_page.dart
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../services/db_helper.dart';
-import '../providers/network_provider.dart';
-import 'package:syncfusion_flutter_charts/charts.dart'; // Certifique-se de adicionar no pubspec.yaml
+import 'package:syncfusion_flutter_charts/charts.dart';
+import '../services/device_service.dart';
+import '../models/device_traffic.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -13,76 +12,49 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-  late List<DeviceTraffic> devicesTraffic;
-  late TooltipBehavior _tooltipBehavior;
+  final DeviceService _deviceService = DeviceService();
+  bool _loading = true;
+  List<DeviceTraffic> _trafficData = [];
 
   @override
   void initState() {
     super.initState();
-    _tooltipBehavior = TooltipBehavior(enable: true);
-    devicesTraffic = [];
-    loadTrafficData();
+    _loadTrafficData();
   }
 
-  Future<void> loadTrafficData() async {
-    final networkProvider = Provider.of<NetworkProvider>(context, listen: false);
-    final db = DBHelper();
-
-    // Carregar tráfego real do banco
-    final trafficData = await db.last7Days(); // Certifique-se que DBHelper tem last7Days implementado corretamente
-    setState(() {
-      devicesTraffic = trafficData.map((data) {
-        final device = networkProvider.getDeviceByMac(data.mac);
-        return DeviceTraffic(
-          name: device?.name ?? data.mac,
-          rxBytes: data.rxBytes,
-          txBytes: data.txBytes,
-        );
-      }).toList();
-    });
+  Future<void> _loadTrafficData() async {
+    setState(() => _loading = true);
+    _trafficData = await _deviceService.getTrafficDataLast7Days();
+    setState(() => _loading = false);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: SfCartesianChart(
-          tooltipBehavior: _tooltipBehavior,
-          primaryXAxis: CategoryAxis(),
-          series: <ChartSeries>[
-            ColumnSeries<DeviceTraffic, String>(
-              dataSource: devicesTraffic,
-              xValueMapper: (DeviceTraffic dt, _) => dt.name,
-              yValueMapper: (DeviceTraffic dt, _) => dt.rxBytes,
-              name: 'Recebido (Bytes)',
-              color: Colors.blue,
+      appBar: AppBar(title: const Text('Dashboard')),
+      body: _loading
+          ? const Center(child: CircularProgressIndicator())
+          : SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              primaryYAxis: NumericAxis(),
+              tooltipBehavior: TooltipBehavior(enable: true),
+              series: <ChartSeries>[
+                ColumnSeries<DeviceTraffic, String>(
+                  dataSource: _trafficData,
+                  xValueMapper: (DeviceTraffic data, _) => data.day,
+                  yValueMapper: (DeviceTraffic data, _) => data.rxBytes,
+                  name: 'Download',
+                  color: Colors.blue,
+                ),
+                ColumnSeries<DeviceTraffic, String>(
+                  dataSource: _trafficData,
+                  xValueMapper: (DeviceTraffic data, _) => data.day,
+                  yValueMapper: (DeviceTraffic data, _) => data.txBytes,
+                  name: 'Upload',
+                  color: Colors.green,
+                ),
+              ],
             ),
-            ColumnSeries<DeviceTraffic, String>(
-              dataSource: devicesTraffic,
-              xValueMapper: (DeviceTraffic dt, _) => dt.name,
-              yValueMapper: (DeviceTraffic dt, _) => dt.txBytes,
-              name: 'Enviado (Bytes)',
-              color: Colors.green,
-            ),
-          ],
-        ),
-      ),
     );
   }
-}
-
-class DeviceTraffic {
-  final String name;
-  final int rxBytes;
-  final int txBytes;
-
-  DeviceTraffic({
-    required this.name,
-    required this.rxBytes,
-    required this.txBytes,
-  });
 }
