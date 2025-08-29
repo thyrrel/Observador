569f29dd74738ed7fead43e207b7f5b9763876ba
thyrrel <thyrrel@gmail.com>
2025-08-29 01:30:35 -0300

    Update dashboard_page.dart

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,60 +1,47 @@
-// lib/pages/dashboard_page.dart
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import '../services/device_service.dart';
-import '../models/device_traffic.dart';
+import '../services/ia_network_service.dart';
+import '../services/router_service.dart';
+import '../models/device_model.dart';
 
 class DashboardPage extends StatefulWidget {
-  const DashboardPage({Key? key}) : super(key: key);
+  final IANetworkService iaService;
+
+  DashboardPage({required this.iaService});
 
   @override
   _DashboardPageState createState() => _DashboardPageState();
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-  final DeviceService _deviceService = DeviceService();
-  bool _loading = true;
-  List<DeviceTraffic> _trafficData = [];
+  List<DeviceModel> devices = [];
+  Map<String, double> trafficUsage = {};
 
   @override
   void initState() {
     super.initState();
-    _loadTrafficData();
-  }
-
-  Future<void> _loadTrafficData() async {
-    setState(() => _loading = true);
-    _trafficData = await _deviceService.getTrafficDataLast7Days();
-    setState(() => _loading = false);
+    // Inicializar lista de dispositivos e tráfego
+    devices = widget.iaService.devices;
+    trafficUsage = {}; // Será preenchido com dados reais
   }
 
   @override
   Widget build(BuildContext context) {
+    widget.iaService.updateDevices(devices);
+    widget.iaService.analyzeTraffic(trafficUsage);
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard')),
-      body: _loading
-          ? const Center(child: CircularProgressIndicator())
-          : SfCartesianChart(
-              primaryXAxis: CategoryAxis(),
-              primaryYAxis: NumericAxis(),
-              tooltipBehavior: TooltipBehavior(enable: true),
-              series: <ChartSeries>[
-                ColumnSeries<DeviceTraffic, String>(
-                  dataSource: _trafficData,
-                  xValueMapper: (DeviceTraffic data, _) => data.day,
-                  yValueMapper: (DeviceTraffic data, _) => data.rxBytes,
-                  name: 'Download',
-                  color: Colors.blue,
-                ),
-                ColumnSeries<DeviceTraffic, String>(
-                  dataSource: _trafficData,
-                  xValueMapper: (DeviceTraffic data, _) => data.day,
-                  yValueMapper: (DeviceTraffic data, _) => data.txBytes,
-                  name: 'Upload',
-                  color: Colors.green,
-                ),
-              ],
-            ),
+      appBar: AppBar(title: Text("Dashboard")),
+      body: ListView.builder(
+        itemCount: devices.length,
+        itemBuilder: (context, index) {
+          var device = devices[index];
+          double mbps = trafficUsage[device.ip] ?? 0;
+          return ListTile(
+            title: Text('${device.name} (${device.type})'),
+            subtitle: Text('Uso: $mbps Mbps'),
+          );
+        },
+      ),
     );
   }
 }
