76e546b87fe591eed0440acfb8debc4f50c26089
thyrrel <thyrrel@gmail.com>
2025-08-29 02:49:04 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+import 'screens/dashboard_screen.dart';
 import 'services/router_service.dart';
-import 'services/ia_service.dart';
-import 'screens/home_screen.dart';
+import 'services/ia_network_service.dart';
 
-void main() {
-  runApp(ObservadorApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDocDir.path);
+
+  final routerService = RouterService();
+  final iaService = IANetworkService(
+    voiceCallback: (msg) => print('IA: $msg'),
+    routerService: routerService,
+  );
+
+  await iaService.initHive();
+
+  runApp(MyApp(routerService: routerService, iaService: iaService));
 }
 
-class ObservadorApp extends StatefulWidget {
-  @override
-  State<ObservadorApp> createState() => _ObservadorAppState();
-}
+class MyApp extends StatelessWidget {
+  final RouterService routerService;
+  final IANetworkService iaService;
 
-class _ObservadorAppState extends State<ObservadorApp> {
-  late RouterService routerService;
-  late IAService iaService;
-
-  @override
-  void initState() {
-    super.initState();
-    routerService = RouterService();
-    iaService = IAService(
-      routerService: routerService,
-      voiceCallback: (msg) {
-        // Aqui você pode usar TTS ou log
-        print('IA: $msg');
-      },
-    );
-  }
+  const MyApp({required this.routerService, required this.iaService, super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Observador',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(
-        iaService: iaService,
-        routerService: routerService,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: DashboardScreen(routerService: routerService, iaService: iaService),
     );
   }
 }
