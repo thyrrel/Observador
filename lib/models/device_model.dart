bcfe6dab5bf6396a62db8f2ee72c9112d8b7a04d
thyrrel <thyrrel@gmail.com>
2025-08-29 17:10:07 -0300

    Update device_model.dart

--- lib/models/device_model.dart
+++ lib/models/device_model.dart
@@ -1,15 +1,15 @@
-class RouterDevice {
+class DeviceModel {
+  final String ip;
   final String mac;
   final String name;
-  final int rxBytes;
-  final int txBytes;
-  final bool blocked;
+  final String type;
+  bool isBlocked;
 
-  RouterDevice({
+  DeviceModel({
+    required this.ip,
     required this.mac,
     required this.name,
-    required this.rxBytes,
-    required this.txBytes,
-    required this.blocked,
+    required this.type,
+    this.isBlocked = false,
   });
 }
