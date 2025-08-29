82fc6b4a76dcad804bd6cfaec2978f85bc5a037b
thyrrel <thyrrel@gmail.com>
2025-08-29 02:10:13 -0300

    Update device_model.dart

--- lib/models/device_model.dart
+++ lib/models/device_model.dart
@@ -1,15 +1,47 @@
+// device_model.dart
 class DeviceModel {
-  final String ip;
-  final String mac;
-  final String manufacturer;
-  final String name;
-  final String type;
+  String ip;
+  String mac;
+  String manufacturer;
+  String type;
+  String name;
 
   DeviceModel({
     required this.ip,
     required this.mac,
     required this.manufacturer,
-    required this.name,
     required this.type,
+    required this.name,
   });
+
+  // Converte para Map, útil para integração com RouterService
+  Map<String, dynamic> toMap() {
+    return {
+      'ip': ip,
+      'mac': mac,
+      'manufacturer': manufacturer,
+      'type': type,
+      'name': name,
+    };
+  }
+
+  // Cria DeviceModel a partir de Map
+  factory DeviceModel.fromMap(Map<String, dynamic> map) {
+    return DeviceModel(
+      ip: map['ip'] ?? '',
+      mac: map['mac'] ?? '',
+      manufacturer: map['manufacturer'] ?? 'Desconhecido',
+      type: map['type'] ?? '',
+      name: map['name'] ?? '',
+    );
+  }
+
+  // Atualiza dados de dispositivo
+  void updateFrom(DeviceModel other) {
+    ip = other.ip;
+    mac = other.mac;
+    manufacturer = other.manufacturer;
+    type = other.type;
+    name = other.name;
+  }
 }
