3fdfe61cc49c0a0f151809c37ed283597a6eea25
thyrrel <thyrrel@gmail.com>
2025-08-29 02:23:29 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,44 @@
 import 'package:flutter/material.dart';
-import 'screens/dashboard_screen.dart';
-import 'screens/routers_screen.dart';
 import 'services/router_service.dart';
+import 'services/ia_service.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  final routerService = RouterService();
-  runApp(MyApp(routerService: routerService));
+  runApp(ObservadorApp());
 }
 
-class MyApp extends StatelessWidget {
-  final RouterService routerService;
+class ObservadorApp extends StatefulWidget {
+  @override
+  State<ObservadorApp> createState() => _ObservadorAppState();
+}
 
-  const MyApp({super.key, required this.routerService});
+class _ObservadorAppState extends State<ObservadorApp> {
+  late RouterService routerService;
+  late IAService iaService;
+
+  @override
+  void initState() {
+    super.initState();
+    routerService = RouterService();
+    iaService = IAService(
+      routerService: routerService,
+      voiceCallback: (msg) {
+        // Aqui você pode usar TTS ou log
+        print('IA: $msg');
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Observador',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomeScreen(routerService: routerService),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  final RouterService routerService;
-
-  const HomeScreen({super.key, required this.routerService});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Observador')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => DashboardScreen(routerService: routerService)),
-              ),
-              child: const Text('Dashboard'),
-            ),
-            ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => RoutersScreen(routerService: routerService)),
-              ),
-              child: const Text('Roteadores'),
-            ),
-          ],
-        ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(
+        iaService: iaService,
+        routerService: routerService,
       ),
     );
   }
