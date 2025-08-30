253d6b30e152d0859b2c2ff83ba1f6d37daa3afe
thyrrel <thyrrel@gmail.com>
2025-08-30 20:34:59 -0300

    Update remote_ai_service.dart

--- lib/services/remote_ai_service.dart
+++ lib/services/remote_ai_service.dart
@@ -1,11 +1,16 @@
 import 'api_handler.dart';
 import 'logger_service.dart';
+import 'storage_service.dart';
 
 class RemoteAIService {
   final APIHandler apiHandler;
+  final StorageService storageService;
   final LoggerService logger = LoggerService();
 
-  RemoteAIService(this.apiHandler);
+  RemoteAIService({
+    required this.apiHandler,
+    required this.storageService,
+  });
 
   Future<void> analyzeLogsRemotely(List<String> logs) async {
     try {
