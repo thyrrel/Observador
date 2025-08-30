e7db839e056c6851c6ef720849692914e9575ba1
thyrrel <thyrrel@gmail.com>
2025-08-30 01:12:05 -0300

    Update router_service.dart

--- lib/services/router_service.dart
+++ lib/services/router_service.dart
@@ -1,26 +1,14 @@
 // lib/services/router_service.dart
-import 'device_model.dart';
+import '../models/device_model.dart';
 
 class RouterService {
   Future<List<DeviceModel>> getConnectedDevices() async {
-    // Simulação: buscar dispositivos reais de roteador
+    // Aqui normalmente você chamaria a API do roteador
     return [];
   }
 
-  Future<double> getDeviceTraffic(String mac) async {
-    // Simulação: obter tráfego real
-    return 0.0;
-  }
-
-  Future<void> prioritizeDevice(String mac, {int priority = 200}) async {
-    // Configura QoS
-  }
-
-  Future<void> blockDevice(String ip, String mac) async {
-    // Bloqueia dispositivo
-  }
-
-  Future<void> limitDevice(String ip, String mac, int limitKbps) async {
-    // Limita banda
-  }
+  Future<void> blockDevice(String ip, String mac) async {}
+  Future<void> limitDevice(String ip, String mac, int limitKbps) async {}
+  Future<void> prioritizeDevice(String ip, String mac, {int priority = 200}) async {}
+  Future<void> updateDevice(DeviceModel device) async {}
 }
