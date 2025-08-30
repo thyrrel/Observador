dbada2e14b26d143e178c403b6f79aebd6f66d93
thyrrel <thyrrel@gmail.com>
2025-08-30 20:23:39 -0300

    Update traffic_worker.dart

--- lib/workers/traffic_worker.dart
+++ lib/workers/traffic_worker.dart
@@ -1,10 +1,13 @@
 import 'package:workmanager/workmanager.dart';
-import 'package:observador/services/traffic_collector.dart';
+import 'package:observador/services/traffic_collector_service.dart';
 
 @pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
-    await TrafficCollector().collectOnce();
+    final collector = TrafficCollectorService();
+    collector.startMonitoring((data) {
+      // TODO: processar uso de tráfego
+    });
     return true;
   });
 }
