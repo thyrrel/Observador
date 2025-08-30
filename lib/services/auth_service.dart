534b8c83c78160ba343b4496f636395e9f493e45
thyrrel <thyrrel@gmail.com>
2025-08-30 00:53:56 -0300

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,4 +1,3 @@
-// lib/services/auth_service.dart
 import 'package:local_auth/local_auth.dart';
 
 class AuthService {
@@ -10,7 +9,7 @@ class AuthService {
       bool isDeviceSupported = await _auth.isDeviceSupported();
       if (!canCheckBiometrics || !isDeviceSupported) return false;
 
-      final bool didAuthenticate = await _auth.authenticate(
+      return await _auth.authenticate(
         localizedReason: 'Autentique-se para continuar',
         options: const AuthenticationOptions(
           stickyAuth: true,
@@ -18,7 +17,6 @@ class AuthService {
           biometricOnly: false,
         ),
       );
-      return didAuthenticate;
     } catch (e) {
       return false;
     }
