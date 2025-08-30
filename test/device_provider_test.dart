a0cd8c6cd2b51986d207f06c784c7cfb0231f376
thyrrel <thyrrel@gmail.com>
2025-08-30 01:48:33 -0300

    Create device_provider_test.dart

---
+++ test/device_provider_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:observador/providers/device_provider.dart';
+import 'package:observador/services/network_service.dart';
+
+class MockNetworkService extends NetworkService {
+  @override
+  List<NetworkDevice> get devices => [
+        NetworkDevice(ip: '192.168.0.2', mac: 'AA:BB:CC:DD:EE:01', name: 'PC1', blocked: false),
+        NetworkDevice(ip: '192.168.0.3', mac: 'AA:BB:CC:DD:EE:02', name: 'Phone', blocked: true),
+      ];
+}
+
+void main() {
+  test('DeviceProvider lista de dispositivos', () {
+    final service = MockNetworkService();
+    final provider = DeviceProvider(service);
+    expect(provider.devices.length, 2);
+    expect(provider.devices[0].name, 'PC1');
+    expect(provider.devices[1].blocked, true);
+  });
+}
