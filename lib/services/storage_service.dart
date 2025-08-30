3e0b60089ab5eccc56bc27a54f71cf12ca153d77
thyrrel <thyrrel@gmail.com>
2025-08-30 00:40:36 -0300

    Create storage_service.dart

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,10 @@
+// lib/services/storage_service.dart
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class StorageService {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  Future<void> write(String key, String value) async => await _storage.write(key: key, value: value);
+  Future<String?> read(String key) async => await _storage.read(key: key);
+  Future<void> delete(String key) async => await _storage.delete(key: key);
+}
