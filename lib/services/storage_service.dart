b0f22142d0033d93685c81b34194d4becf3bc038
thyrrel <thyrrel@gmail.com>
2025-08-30 01:14:33 -0300

    Update storage_service.dart

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -4,7 +4,19 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class StorageService {
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
-  Future<void> write(String key, String value) async => await _storage.write(key: key, value: value);
-  Future<String?> read(String key) async => await _storage.read(key: key);
-  Future<void> delete(String key) async => await _storage.delete(key: key);
+  Future<void> write(String key, String value) async {
+    await _storage.write(key: key, value: value);
+  }
+
+  Future<String?> read(String key) async {
+    return await _storage.read(key: key);
+  }
+
+  Future<void> delete(String key) async {
+    await _storage.delete(key: key);
+  }
+
+  Future<void> clear() async {
+    await _storage.deleteAll();
+  }
 }
