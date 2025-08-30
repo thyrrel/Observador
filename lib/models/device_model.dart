83e9fb9a10892feeb4617ed66bb2a665aba69d70
thyrrel <thyrrel@gmail.com>
2025-08-29 21:23:18 -0300

    Update device_model.dart

--- lib/models/device_model.dart
+++ lib/models/device_model.dart
@@ -1,15 +1,35 @@
 class DeviceModel {
-  final String ip;
-  final String mac;
-  final String name;
-  final String type;
-  bool isBlocked;
+  final String id;
+  String name;
+  String ip;
+  bool blocked;
+  String? icon;
 
   DeviceModel({
-    required this.ip,
-    required this.mac,
+    required this.id,
     required this.name,
-    required this.type,
-    this.isBlocked = false,
+    required this.ip,
+    this.blocked = false,
+    this.icon,
   });
+
+  factory DeviceModel.fromJson(Map<String, dynamic> json) {
+    return DeviceModel(
+      id: json['id'],
+      name: json['name'],
+      ip: json['ip'],
+      blocked: json['blocked'] ?? false,
+      icon: json['icon'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'ip': ip,
+      'blocked': blocked,
+      'icon': icon,
+    };
+  }
 }
