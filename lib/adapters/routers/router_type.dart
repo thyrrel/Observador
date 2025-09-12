84cc11077e152007a793a9dd5c429286873727f3
thyrrel <thyrrel@gmail.com>
2025-09-12 03:39:38 -0300

    Create router_type.dart

---
+++ lib/adapters/routers/router_type.dart
@@ -0,0 +1,13 @@
+// /lib/adapters/routers/router_type.dart
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ 🧭 RouterType - Enum dos tipos suportados     ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+enum RouterType {
+  Xiaomi,
+  TPLink,
+  Huawei,
+  Intelbras,
+  MikroTik,
+  Ubiquiti,
+}
