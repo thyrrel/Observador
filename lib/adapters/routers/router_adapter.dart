44bd950a23156b9f699f929d5538758dc2fed5e1
thyrrel <thyrrel@gmail.com>
2025-09-12 03:38:13 -0300

    Create router_adapter.dart

---
+++ lib/adapters/routers/router_adapter.dart
@@ -0,0 +1,16 @@
+// /lib/adapters/routers/router_adapter.dart
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ 🔌 RouterAdapter - Interface base para adaptadores ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+import '../../models/device_model.dart';
+import 'router_session.dart';
+
+abstract class RouterAdapter {
+  Future<RouterSession?> login(String ip, String username, String password);
+  Future<List<RouterDevice>> getClients(String ip, String token);
+  Future<bool> blockDevice(String ip, String token, String mac);
+  Future<bool> unblockDevice(String ip, String token, String mac);
+  Future<bool> limitDevice(String ip, String token, String mac, int limit);
+  Future<bool> removeLimit(String ip, String token, String mac);
+}
